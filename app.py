7a05e0f6ba4d054d6f75b48088e169ae34c01137
rjachuthan <riturajachuthan@gmail.com>
2024-02-26 21:48:29 +0530

    Merge Conflict

--- app.py
+++ app.py
@@ -7,17 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-<<<<<<< HEAD
     api_address: str = "https://civitai.com/api/v1/images?nsfw=None&limit=12&period=Day"
-
-    data = requests.get(api_address).json()
-    image_urls = [x["url"] for x in data["items"]]
-    return render_template("index.html", data=image_urls)
-=======
-    api_address: str = "https://civitai.com/api/v1/images?nsfw=None&limit=24&period=Week"
     data = requests.get(api_address).json()
     return render_template("index.html", data=data)
->>>>>>> feature/readfromapi
 
 
 if __name__ == "__main__":
