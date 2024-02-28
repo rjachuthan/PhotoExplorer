3f5d09178bdcca3d312653e8f0db1dd7a9a468d5
rjachuthan <riturajachuthan@gmail.com>
2024-02-28 21:55:55 +0530

    Refactor app.py to include new functionality for retrieving and rendering images from the Civitai API

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, render_template_string, request
 
 app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
+def home():
+    period = request.form.get("periodoptions") or "Day"
+    sort = request.form.get("sortoptions") or "Newest"
+    nsfw = request.form.get("nsfwoptions") or "None"
+
+    api_address: str = (
+        f"https://civitai.com/api/v1/images?period={period}&nsfw={nsfw}&sort={sort}"
+    )
+    data = requests.get(api_address).json()
+    return render_template("index.html", data=data)
+
+
 @app.route("/civitai/images", methods=["POST"])
 def civitimages():
     period = request.form.get("periodoptions") or "Day"
     sort = request.form.get("sortoptions") or "Newest"
     nsfw = request.form.get("nsfwoptions") or "None"
 
-    api_address: str = f"https://civitai.com/api/v1/images?period={period}&nsfw={nsfw}&sort={sort}&limit=4"
+    api_address: str = (
+        f"https://civitai.com/api/v1/images?period={period}&nsfw={nsfw}&sort={sort}"
+    )
     data = requests.get(api_address).json()
-    return render_template("index.html", data=data)
+    rendered_template = render_template_string(
+        """
+            {% import "components/cards.html" as cards %}
+            {% for item in data['items'] %}
+                {{ cards.image_box(url=item.url, stats=item['stats']) }}
+            {% endfor %}
+        """,
+        data=data,
+    )
+    print(api_address)
+    return rendered_template
 
 
 @app.route("/civitai/models")
