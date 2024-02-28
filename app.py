a609d62a5f69849a96acca3e35b458f7f2588f6c
rjachuthan <riturajachuthan@gmail.com>
2024-02-28 21:23:57 +0530

    Update default values for period, sort, and nsfw options

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__, template_folder="templates")
 @app.route("/")
 @app.route("/civitai/images", methods=["POST"])
 def civitimages():
-    period = request.form.get("periodoptions")
-    sort = request.form.get("sortoptions")
-    nsfw = request.form.get("nsfwoptions")
+    period = request.form.get("periodoptions") or "Day"
+    sort = request.form.get("sortoptions") or "Newest"
+    nsfw = request.form.get("nsfwoptions") or "None"
 
     api_address: str = f"https://civitai.com/api/v1/images?period={period}&nsfw={nsfw}&sort={sort}&limit=4"
     data = requests.get(api_address).json()
