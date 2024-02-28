4f62626ff7b47ffff9050686cd3deeef935184e9
rjachuthan <riturajachuthan@gmail.com>
2024-02-28 21:09:27 +0530

    Add routes for retrieving images and models from Civitai API

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
-@app.route("/images")
-def index():
-    api_address: str = "https://civitai.com/api/v1/images?period=Day&nsfw=None"
+@app.route("/civitai/images", methods=["POST"])
+def civitimages():
+    period = request.form.get("periodoptions")
+    sort = request.form.get("sortoptions")
+    nsfw = request.form.get("nsfwoptions")
+
+    api_address: str = f"https://civitai.com/api/v1/images?period={period}&nsfw={nsfw}&sort={sort}&limit=4"
+    data = requests.get(api_address).json()
+    return render_template("index.html", data=data)
+
+
+@app.route("/civitai/models")
+def civitmodels():
+    api_address: str = "https://civitai.com/api/v1/models?limit=1"
     data = requests.get(api_address).json()
     return render_template("index.html", data=data)
 
