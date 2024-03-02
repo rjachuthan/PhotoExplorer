ddd6b4b8fea24bacdd95f412cb42e6f400f07805
rjachuthan <riturajachuthan@gmail.com>
2024-03-02 16:19:06 +0530

    Setup Navigation

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
+@app.route("/civitai/images")
 def home():
     period = request.form.get("periodoptions") or "Day"
     sort = request.form.get("sortoptions") or "Newest"
