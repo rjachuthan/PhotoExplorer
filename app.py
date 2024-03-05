e308f3830b55680bba57239219d343f1330546cd
rjachuthan <riturajachuthan@gmail.com>
2024-03-05 23:30:56 +0530

    Add CivitaiAPI enum and add Not Found image

--- app.py
+++ app.py
@@ -1,49 +1,61 @@
+from enum import Enum
+
 import requests
 from flask import Flask, render_template, render_template_string, request
 
 app = Flask(__name__, template_folder="templates")
 
 
-@app.route("/")
-@app.route("/civitai/images")
-def home():
-    period = request.form.get("periodoptions") or "Day"
-    sort = request.form.get("sortoptions") or "Newest"
-    nsfw = request.form.get("nsfwoptions") or "None"
+class CivitaiAPI(Enum):
+    """Enum class representing the available API endpoints for Civitai."""
 
-    api_address: str = (
-        f"https://civitai.com/api/v1/images?period={period}&nsfw={nsfw}&sort={sort}"
-    )
-    data = requests.get(api_address).json()
-    return render_template("index.html", data=data)
+    IMAGES = "https://civitai.com/api/v1/images"
+    MODELS = "https://civitai.com/api/v1/models"
 
 
-@app.route("/civitai/images", methods=["POST"])
+@app.route("/", methods=["GET"])
+@app.route("/civitai/images", methods=["GET", "POST"])
 def civitimages():
-    period = request.form.get("periodoptions") or "Day"
-    sort = request.form.get("sortoptions") or "Newest"
-    nsfw = request.form.get("nsfwoptions") or "None"
+    if request.method == "GET":
+        data = requests.get(CivitaiAPI.IMAGES.value).json()
+        return render_template("index.html", data=data)
 
-    api_address: str = (
-        f"https://civitai.com/api/v1/images?period={period}&nsfw={nsfw}&sort={sort}"
-    )
-    data = requests.get(api_address).json()
-    rendered_template = render_template_string(
-        """
-            {% import "components/cards.html" as cards %}
-            {% for item in data['items'] %}
-                {{ cards.image_box(url=item.url, stats=item['stats']) }}
-            {% endfor %}
-        """,
-        data=data,
-    )
-    print(api_address)
-    return rendered_template
+    if request.method == "POST":
+        period = request.form.get("periodoptions") or "Day"
+        sort = request.form.get("sortoptions") or "Newest"
+        nsfw = request.form.get("nsfwoptions") or "None"
+
+        url = CivitaiAPI.IMAGES.value
+        api_address: str = f"{url}?period={period}&nsfw={nsfw}&sort={sort}"
+
+        data = requests.get(api_address).json()
+        rendered_template = render_template_string(
+            """
+                {% import "components/cards.html" as cards %}
+                {% for item in data['items'] %}
+                    {{ cards.image_box(url=item.url, stats=item['stats']) }}
+                {% endfor %}
+            """,
+            data=data,
+        )
+
+        return rendered_template
 
 
 @app.route("/civitai/models", methods=["GET", "POST"])
 def civitmodels():
-    api_address: str = "https://civitai.com/api/v1/models"
+    """
+    This function handles the '/civitai/models' route, which supports both GET
+    and POST requests. For GET requests, it retrieves data from the
+    'https://civitai.com/api/v1/models' API and renders the 'civitmodels.html'
+    template with the data. For POST requests, it processes the form data,
+    constructs the API address with query parameters, retrieves data from the
+    API, and renders the template with the extracted data.
+
+    Returns:
+        The rendered template for the models with the data.
+    """
+    api_address: str = CivitaiAPI.MODELS.value
 
     if request.method == "GET":
         data = requests.get(api_address).json()
@@ -70,13 +82,13 @@ def civitmodels():
 
         extracted_data = []
         for item in data["items"]:
-            url = ""
+            url = "/static/img/image_not_found.png"
             imagesection = item["modelVersions"][0]["images"]
             if len(imagesection) > 0:
                 try:
                     url = imagesection[0]["url"]
                 except KeyError:
-                    url = ""
+                    url = "/static/img/image_not_found.png"
             extracted_item = {
                 "name": item["name"],
                 "creator": item["creator"],
